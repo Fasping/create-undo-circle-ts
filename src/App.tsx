265c2f278b562c8cfe3e8a4cd6c3ace92e627141
Fernando Cases <nandocasesgarcia@gmail.com>
2022-11-27 22:32:20 +0100

    Add new button undo points

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,48 @@
-import './App.css'
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 
 type TPoint = {
   x: number;
   y: number;
-}
-
+};
 
 function App() {
   const [points, setPoint] = useState<TPoint[]>([]);
 
   function handlePlaceCircle(e: React.MouseEvent<HTMLDivElement>) {
     const { clientX, clientY } = e;
-    setPoint([...points, {
-      x: clientX,
-      y: clientY,
-    }])
+    setPoint([
+      ...points,
+      {
+        x: clientX,
+        y: clientY,
+      },
+    ]);
   }
 
-  
+  function handleUndo() {
+    //TODO remove the last point add in the array
+    const newPoints = [...points];
+    newPoints.pop();
+    setPoint(newPoints);
+  }
+
   return (
-    <div className="App" onClick={handlePlaceCircle}>
-      {points.map((point) => (
-        <div className="point" style={{
-          left: point.x - 5 + "px",
-          top: point.y - 5 + "px",
-        }}
-        >
-        </div>
-      ))}
+    <div>
+      <button onClick={handleUndo}>Undo</button>
+      <div className="App" onClick={handlePlaceCircle}>
+        {points.map((point) => (
+          <div
+            className="point"
+            style={{
+              left: point.x - 5 + "px",
+              top: point.y - 5 + "px",
+            }}
+          ></div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
